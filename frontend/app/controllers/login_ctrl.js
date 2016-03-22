5ca16db103a1332e36969df0520e8e69b87363a2
russellvaughan <russellvaughan@gmail.com>
2016-03-22 13:08:41 +0000

    error handling added to login controller

--- frontend/app/controllers/login_ctrl.js
+++ frontend/app/controllers/login_ctrl.js
@@ -1,7 +1,14 @@
 angular.module('makersbnb').controller('LoginController', function($scope,auth){	
  $scope.login = function() {
  	if ($scope.loginForm.$valid) {
- 		auth.login($scope.user);
+ 		var promise = auth.login($scope.user);
+ 		promise.then(success, error);
  	}
- };
+ }; 
+
+ var success = function() {};
+
+ var error = function(response) {
+  $scope.wrongCredentials = true;
+   }; 
 });
